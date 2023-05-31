10a9b915970015657e8224973250db20670ae165
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-05-31 15:16:15 +0900

    Design: 마이페이지 기본 레이아웃 설정

--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,11 +1,30 @@
+import axios from 'axios';
 import styles from './Mypage.module.scss'
+import { useEffect, useState } from 'react';
 
 function Mypage() {
   return (
     <div>
-      <div>Mypage</div>
-      <div>Side navigation</div>
-      <div className={styles.container}>main</div>
+      <div className={styles.title}>Mypage</div>
+      <div className={styles.container}>
+        <div className={styles.sideNavigation}>
+          <ul>
+            <li>
+              내 정보
+            </li>
+            <li>
+              내 게시물
+            </li>
+            <li>
+              뱃지
+            </li>
+            <li>
+              로그아웃
+            </li>
+          </ul>
+        </div>
+        <div className={styles.main}>main</div>
+      </div>
     </div>
   )
 }
