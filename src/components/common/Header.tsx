8a276e42f9f9f6d178db5c471626b98510c04b62
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-05-31 17:28:58 +0900

    Feat: 헤더 기본 레이아웃 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,35 @@
+import { NavLink, Link } from 'react-router-dom';
+import styles from './Header.module.scss';
+
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <div className={styles.inner}>
+        <div>
+          <Link to='/' className={styles.logo}>
+            로고
+          </Link>
+          <ul className={styles.menu}>
+            <li>
+              <NavLink to='/home' className={({ isActive }) => (isActive ? 'active' : '')}>
+                기록하기
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to='/community' className={({ isActive }) => (isActive ? 'active' : '')}>
+                커뮤니티
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Link to='/login' className={styles.button}>
+            시작하기
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
