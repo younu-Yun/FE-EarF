8dfbaae41186d026062df4f5a5b83f8ffc3646b5
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-06-01 18:30:39 +0900

    Feat: SideNav 기능 구현

--- src/components/mypage/common/SideNav.tsx
+++ src/components/mypage/common/SideNav.tsx
@@ -1,23 +1,30 @@
-import styles from './SideNav.module.scss'
-
+import styles from './SideNav.module.scss';
+import { NavLink } from 'react-router-dom';
 function SideNav() {
   return (
     <div className={styles.sideNavigation}>
       <ul>
-        {/*
         <li>
-          <NavLink to="/mypage" className={({ isActive, isPending }) => isPending ? "pending" : isActive ? "active" : "" }>
+          <NavLink
+            to='/mypage/info'
+            className={({ isActive, isPending }) => (isPending ? styles.inactive : isActive ? styles.active : '')}
+          >
             내 정보
           </NavLink>
         </li>
-        */}
-        <li>내 정보</li>
-        <li>내 게시물</li>
+        <li>
+          <NavLink
+            to='/mypage/mycommunity'
+            className={({ isActive, isPending }) => (isPending ? styles.inactive : isActive ? styles.active : '')}
+          >
+            내 게시물
+          </NavLink>
+        </li>
         <li>뱃지</li>
         <li>로그아웃</li>
       </ul>
     </div>
-  )
+  );
 }
 
 export default SideNav;
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,7 +1,7 @@
-import styles from './Mypage.module.scss'
-import Title from 'components/mypage/common/Title'
-import SideNav from 'components/mypage/common/SideNav'
-import Main from 'components/mypage/common/Main'
+import styles from './Mypage.module.scss';
+import Title from 'components/mypage/common/Title';
+import SideNav from 'components/mypage/common/SideNav';
+import Main from 'components/mypage/common/Main';
 
 // 마이페이지는 로그인 된 회원만 접근 가능한 페이지
 // 우선 로그인 상황을 가정하고 페이지 작성
@@ -15,7 +15,7 @@ function Mypage() {
         <Main />
       </div>
     </div>
-  )
+  );
 }
 
 export default Mypage;
