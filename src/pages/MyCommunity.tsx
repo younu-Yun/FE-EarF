bb3951f77efed22923e6c371243fc7c05949add9
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-06-01 18:29:00 +0900

    feat: SideNav의 경로 이동 이벤트를 확인하기 위한 임시 페이지 생성

---
+++ src/pages/MyCommunity.tsx
@@ -0,0 +1,13 @@
+import SideNav from 'components/mypage/common/SideNav';
+import Title from 'components/mypage/common/Title';
+function MyCommunity() {
+  return (
+    <div>
+      <Title />
+      <SideNav />
+      MyCommunity
+    </div>
+  );
+}
+
+export default MyCommunity;
