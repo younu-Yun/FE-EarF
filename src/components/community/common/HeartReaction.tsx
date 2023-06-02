50784939a57c55b8c77a2bf543589844a5e05717
gwanseeum <9v.veu.v@gmail.com>
2023-06-02 17:57:57 +0900

    Fix: 좋아요 반응 handler 등록

--- src/components/community/common/HeartReaction.tsx
+++ src/components/community/common/HeartReaction.tsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import { ReactComponent as Heart } from 'assets/icons/Heart.svg';
 import styles from '../questionBoard/QuestionUserReaction.module.scss';
 
 function HeartReaction() {
+  const [likeIt, setLikeIt] = useState(false);
+  const [likeItNumber, setLikeItNumber] = useState(0);
+
+  const handleLikeIt = () => {
+    setLikeIt((prevLikeIt) => !prevLikeIt);
+    setLikeItNumber((prevNumber) => (likeIt ? prevNumber - 1 : prevNumber + 1));
+  };
+
   return (
-    <div className={styles.reactionContainer}>
+    <div className={styles.reactionContainer} onClick={handleLikeIt}>
       <Heart />
-      <span className={styles.reactionNumber}>5</span>
+      <span className={styles.reactionNumber}>{likeItNumber}</span>
     </div>
   );
 }
