9a09ae0df3714d45ec02c40476de1463ad9cd360
gwanseeum <9v.veu.v@gmail.com>
2023-06-02 20:21:54 +0900

    Fix: routes 등록

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,16 @@ import Home from 'pages/Home';
 import MyPage from 'pages/MyPage';
 import MyCommunity from 'pages/MyCommunity';
 import MyPageEdit from 'pages/MyPageEdit';
+import Community from 'pages/Community';
 
 const routes = [
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/community',
+    element: <Community />,
   },
   {
     path: '/mypage/info',
