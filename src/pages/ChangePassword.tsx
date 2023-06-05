ce7a11dbe95218dd3d9848961552f3fc2860c1b4
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-06-06 01:18:29 +0900

    Feat: 비밀번호 변경 기능 추가

---
+++ src/pages/ChangePassword.tsx
@@ -0,0 +1,127 @@
+import styles from './ChangePassword.module.scss';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { DefaultInput } from 'components/User/DefaultInput';
+
+function ChangePassword() {
+  const navigate = useNavigate();
+
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleCurrentPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentPassword(e.target.value);
+  };
+
+  const handleNewPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    /**
+     * 유효성검사
+        현재비밀번호 : 값이 입력되지 않았을때 "비밀번호를 입력해주세요."
+        새로운 비밀번호 : 8자 이하로 입력시 "비밀번호를 8자 이상 입력해주세요."
+        비밀번호 확인 : 새로운 비밀번호와 값이 같아야함. 다를시 "비밀번호가 다릅니다."
+     */
+    if (currentPassword.length === 0 || newPassword.length === 0 || confirmPassword.length === 0) {
+      setShowWarning(true);
+      return;
+    }
+
+    if (newPassword.length < 8) {
+      setShowWarning(true);
+      console.log('비밀번호는 8자 이상이어야 합니다.');
+      return;
+    }
+
+    if (newPassword !== confirmPassword) {
+      setShowWarning(true);
+      console.log('새로운 비밀번호와 확인 비밀번호가 일치하지 않습니다.');
+      return;
+    }
+
+    try {
+      //비밀번호 변경 성공 시
+
+      //임시로 만든 api주소 → 추후 수정예정
+      const response = await axios.post('/api/user/change-password', {
+        currentPassword,
+        newPassword,
+      });
+
+      alert('비밀번호 변경이 완료되었습니다. 다시 로그인해주세요.');
+      console.log(`비밀번호 변경이 완료되었습니다: ${response.data}`);
+
+      navigate('/login');
+    } catch (error) {
+      // 비밀번호 변경 실패 시
+      console.error('비밀번호 변경에 실패했습니다:', error);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <h2>비밀번호 변경</h2>
+      </div>
+      <div className={styles.form}>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>비밀번호 변경</legend>
+
+            <DefaultInput
+              inputProps={{
+                type: 'password',
+                id: 'currentPassword',
+                value: currentPassword,
+                onChange: handleCurrentPasswordChange,
+              }}
+              label='현재 비밀번호'
+              showWarning={showWarning && currentPassword.length === 0}
+              warning='비밀번호를 입력해주세요.'
+            />
+
+            <DefaultInput
+              inputProps={{
+                type: 'password',
+                id: 'newPassword',
+                value: newPassword,
+                onChange: handleNewPasswordChange,
+              }}
+              label='새로운 비밀번호'
+              showWarning={showWarning && newPassword.length < 8}
+              warning='비밀번호를 8자 이상 입력해주세요.'
+            />
+
+            <DefaultInput
+              inputProps={{
+                type: 'password',
+                id: 'confirmPassword',
+                value: confirmPassword,
+                onChange: handleConfirmPasswordChange,
+              }}
+              label='비밀번호 확인'
+              showWarning={showWarning && (confirmPassword.length === 0 || newPassword !== confirmPassword)}
+              warning='비밀번호가 다릅니다.'
+            />
+          </fieldset>
+          <div className={styles.buttonBox}>
+            <button type='submit'>변경하기</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ChangePassword;
