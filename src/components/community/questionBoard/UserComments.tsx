8ce674e98f135e9c0abb3521580ddc0168c68f48
younu-Yun <dbssnfjd122@gmail.com>
2023-06-06 14:52:15 +0900

    Fix: Button 컴포넌트 경로 수정

--- src/components/community/questionBoard/UserComments.tsx
+++ src/components/community/questionBoard/UserComments.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import CommentItem from './CommentItem';
-import Button from '../../common/Button';
+import Button from 'components/common/Button';
 import styles from './UserComments.module.scss';
 
 function UserComments() {
--- src/components/community/questionPosting/QuestionPostingBoard.tsx
+++ src/components/community/questionPosting/QuestionPostingBoard.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import Button from '../../common/Button';
+import Button from 'components/common/Button';
 import PostingContent from './PostingContent';
 import styles from './QuestionPostingBoard.module.scss';
 
