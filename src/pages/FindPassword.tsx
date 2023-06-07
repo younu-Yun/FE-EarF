8a51fc8ad358ae93bc663bcc1c87b7b5ee5a4ae7
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-06-07 18:42:37 +0900

    Fix: 비밀번호 찾기 유효성검사 수정

--- src/pages/FindPassword.tsx
+++ src/pages/FindPassword.tsx
@@ -1,18 +1,43 @@
 import styles from './FindPassword.module.scss';
-import React, { useState } from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
+import { DefaultInput } from 'components/User/DefaultInput';
+import axios from 'axios';
 
 function FindPassword() {
   const [email, setEmail] = useState('');
+  const [emailWarning, setEmailWarning] = useState('');
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-
-  const handleSendEmail = (e: React.FormEvent) => {
+  const handleSendEmail = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    if (!validateEmail(email) || email === '') {
+      setEmailWarning('유효한 이메일을 입력해주세요.');
+      return;
+    }
+
+    setEmailWarning('');
+
+    try {
+      await axios.post('/api/user/reset-password', {
+        email: email,
+      });
+      alert('초기화 된 비밀번호를 발송했습니다.');
+    } catch (error) {
+      alert(`${error.response.data.message}`);
+    }
+
     console.log('Send email to:', email);
+  };
+
+  const validateEmail = (email: string) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setEmailWarning('');
   };
 
   return (
@@ -24,10 +49,17 @@ function FindPassword() {
         <form onSubmit={handleSendEmail}>
           <fieldset>
             <legend>비밀번호 찾기</legend>
-            <div className={styles.formElement}>
-              <label htmlFor='email'>이메일</label>
-              <input type='email' id='email' value={email} onChange={handleEmailChange} />
-            </div>
+            <DefaultInput
+              inputProps={{
+                type: 'email',
+                id: 'email',
+                value: email,
+                onChange: handleEmailChange,
+              }}
+              label='이메일'
+              showWarning={true}
+              warning={emailWarning}
+            />
           </fieldset>
           <div className={styles.buttonBox}>
             <button type='submit'>비밀번호 발송</button>
