1776044e981ccaffb10a249d8c00de4832571611
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-06-09 05:20:55 +0900

    Fix: 비밀번호 찾기 레이아웃 수정

--- src/pages/FindPassword.tsx
+++ src/pages/FindPassword.tsx
@@ -3,6 +3,7 @@ import React, { useState, ChangeEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { DefaultInput } from 'components/User/DefaultInput';
 import axios from 'axios';
+import JoginIllust from '../assets/images/JoinIllust.jpg';
 
 function FindPassword() {
   const [email, setEmail] = useState('');
@@ -24,7 +25,8 @@ function FindPassword() {
       });
       alert('초기화 된 비밀번호를 발송했습니다.');
     } catch (error) {
-      alert(`${error.response.data.message}`);
+      alert('이메일 발송 중 오류가 발생했습니다.');
+      console.log(`이메일 발송 중 오류가 발생했습니다. ${error}`);
     }
 
     console.log('Send email to:', email);
@@ -42,30 +44,39 @@ function FindPassword() {
 
   return (
     <div className={styles.container}>
-      <div className={styles.title}>
-        <h2>비밀번호 찾기</h2>
-      </div>
-      <div className={styles.form}>
-        <form onSubmit={handleSendEmail}>
-          <fieldset>
-            <legend>비밀번호 찾기</legend>
-            <DefaultInput
-              inputProps={{
-                type: 'email',
-                id: 'email',
-                value: email,
-                onChange: handleEmailChange,
-              }}
-              label='이메일'
-              showWarning={true}
-              warning={emailWarning}
-            />
-          </fieldset>
-          <div className={styles.buttonBox}>
-            <button type='submit'>비밀번호 발송</button>
-            <Link to='/login'>로그인</Link>
-          </div>
-        </form>
+      <div>
+        <div className={styles.image}>
+          <img src={JoginIllust} alt='아이디찾기 일러스트' />
+        </div>
+        <div className={styles.form}>
+          <form onSubmit={handleSendEmail}>
+            <fieldset>
+              <legend>비밀번호 찾기</legend>
+              <div className={styles.logo}>
+                <span>EarF</span>
+              </div>
+              <div className={styles.title}>
+                <h2>비밀번호 찾기</h2>
+                <p>이메일 입력시, 임시비밀번호가 발송됩니다.</p>
+              </div>
+              <DefaultInput
+                inputProps={{
+                  type: 'email',
+                  id: 'email',
+                  value: email,
+                  onChange: handleEmailChange,
+                }}
+                label='이메일'
+                showWarning={true}
+                warning={emailWarning}
+              />
+            </fieldset>
+            <div className={styles.buttonBox}>
+              <Link to='/login'>로그인</Link>
+              <button type='submit'>비밀번호 발송</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
