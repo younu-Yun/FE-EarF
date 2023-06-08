dc25f4cc9afcc7c2ba2a54bdfc264063de9ab8cf
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-06-09 00:35:03 +0900

    Feat: 뱃지 리스트 모듈화 및, Api 요청 테스트 진행코드 작성

--- src/components/mypage/common/Badge.tsx
+++ src/components/mypage/common/Badge.tsx
@@ -1,70 +1,43 @@
 import { useState } from 'react';
 import styles from './Badge.module.scss';
-import { badgeImg1, badgeImg2, badgeImg3, badgeImg4, badgeImg5, badgeImg6, badgeImg7 } from 'assets/images/badgeIndex';
 import BadgeModal from './BadgeModal';
+import BadgeList from './BadgeList';
 
-interface BadgeInfo {
-  name: string;
-  isGet: boolean;
-}
-
-const BADGE_LIST: BadgeInfo[] = [
-  {
-    name: '신규유저',
-    isGet: true,
-  },
-  {
-    name: '최초 작성',
-    isGet: true,
-  },
-  {
-    name: '3회이상 연속 작성',
-    isGet: true,
-  },
-  {
-    name: '텀블러 사용 3회',
-    isGet: false,
-  },
-  {
-    name: '대중교통 이용 3회',
-    isGet: false,
-  },
-  {
-    name: '채식하기 3회',
-    isGet: true,
-  },
-  {
-    name: '커뮤니티 포스팅 3회',
-    isGet: true,
-  },
-];
-
-const badgeImages = [badgeImg1, badgeImg2, badgeImg3, badgeImg4, badgeImg5, badgeImg6, badgeImg7];
+// 대표 이미지는 모달창을 띄워서 대표이미지 설정 버튼을 누르면, checkedBadge로 전송
+// 커뮤니티에서 프로필을 만들 때 확인해서, 이미지를 적용
 
 function Badge() {
-  const [badges, setBadges] = useState(BADGE_LIST);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [selectedImageIndex, setSelectedImageIndex] = useState<number | null>(null);
 
   // Edit 모달
-  const handleShowModal = (index: number): void => {
-    setSelectedImageIndex(index);
+  const handleShowModal = (index?: number): void => {
+    if (typeof index !== 'undefined') setSelectedImageIndex(index);
     setShowModal((prevState) => !prevState);
   };
-  // user에 저장된 badge를 가져와서 보여줌 useState 사용?
-  // 전체 뱃지 리스트를 먼저 화면에 뿌려줌
-  // 상태 값이 변경됨에 따라 뱃지의 활성 상태를 보여줌
-  // 이를 다시 서버로 전송?? 언제 서버로 데이터를 넘겨줄 지 로직 설정
+
   return (
     <div className={styles.container}>
-      {badges.map((badge, index) => (
-        <div key={index} className={badge.isGet ? styles.items : `${styles.items} ${styles.notAcquired}`}>
+      {BadgeList.map((badge, index) => (
+        <div
+          key={index}
+          className={badge.isGet ? styles.items : `${styles.items} ${styles.notAcquired}`}
+          onClick={() => handleShowModal(index)}
+        >
           <div>{badge.name}</div>
-          <img src={badgeImages[index]} alt='뱃지 이미지' onClick={() => handleShowModal(index)} />
+          <img src={badge.url} alt='뱃지 이미지' />
           <div>{badge.isGet.toString()}</div>
         </div>
       ))}
-      {showModal && selectedImageIndex !== null && <BadgeModal imgSrc={badgeImages[selectedImageIndex]} />}
+      {showModal && selectedImageIndex !== null && (
+        <BadgeModal
+          name={BadgeList[selectedImageIndex].name}
+          imgSrc={BadgeList[selectedImageIndex].url}
+          isGet={BadgeList[selectedImageIndex].isGet}
+          info={BadgeList[selectedImageIndex].info}
+          onClick={handleShowModal}
+        />
+      )}
     </div>
   );
 }
