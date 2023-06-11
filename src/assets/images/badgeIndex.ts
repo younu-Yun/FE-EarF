95b45421877f53895b7b2526db4b2e022aa8176d
[SW4/2반] 윤성준 <tjd1wns15@gmail.com>
2023-06-12 01:33:12 +0900

    Feat: Badge Api 연결 완료

--- src/assets/images/badgeIndex.ts
+++ src/assets/images/badgeIndex.ts
@@ -1,9 +1,9 @@
-import badgeImg1 from './badge1.jpg';
-import badgeImg2 from './badge2.jpg';
-import badgeImg3 from './badge3.jpg';
-import badgeImg4 from './badge4.jpg';
-import badgeImg5 from './badge5.jpg';
-import badgeImg6 from './badge6.jpg';
-import badgeImg7 from './badge7.jpg';
+import badgeImg1 from './badge1.png';
+import badgeImg2 from './badge2.png';
+import badgeImg3 from './badge3.png';
+import badgeImg4 from './badge4.png';
+import badgeImg5 from './badge5.png';
+import badgeImg6 from './badge6.png';
+import badgeImg7 from './badge7.png';
 
 export { badgeImg1, badgeImg2, badgeImg3, badgeImg4, badgeImg5, badgeImg6, badgeImg7 };
