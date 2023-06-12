6a0011901ab7003f92ec8085e27e657c62435dbb
younu-Yun <87592697+younu-Yun@users.noreply.github.com>
2023-06-12 22:00:52 +0900

    Feat: validation 유효성검사 컴포넌트 분리

---
+++ src/components/User/validation.ts
@@ -0,0 +1,38 @@
+interface FormData {
+  id?: string;
+  password?: string;
+  passwordConfirm?: string;
+  name?: string;
+  email?: string;
+  phone?: string;
+}
+
+const validateField = (fieldName: keyof FormData, fieldValue: string, formData: FormData) => {
+  let fieldRegex: RegExp;
+
+  switch (fieldName) {
+    case 'id':
+      fieldRegex = /^[a-z0-9]{6,20}$/;
+      break;
+    case 'password':
+      fieldRegex = /^.{8,}$/;
+      break;
+    case 'passwordConfirm':
+      return fieldValue === formData.password;
+    case 'email':
+      fieldRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      break;
+    case 'phone':
+      fieldRegex = /^\d{3}-\d{4}-\d{4}$/;
+      break;
+    case 'name':
+      fieldRegex = /^.{2,}$/;
+      break;
+    default:
+      return true;
+  }
+
+  return fieldRegex.test(fieldValue);
+};
+
+export { validateField };
